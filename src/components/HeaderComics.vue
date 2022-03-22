9801a079b7415de2ed671924fe8587f1b63d2804
xVal4k <cristianbricicaru.cb@gmail.com>
2022-03-22 16:13:51 +0100

    header done

--- src/components/HeaderComics.vue
+++ src/components/HeaderComics.vue
@@ -1,15 +1,83 @@
 <template>
+<header>
   <div class="container">
-    header
+    <img src="../assets/img/dc-logo.png" alt="logo">
+    <ul>
+      <li v-for="(link, index) in links" :class="activeIndex == index ? 'active_index' : ''" v-bind:key="index" @click="setIndex(index)"><a href="#">{{ link.text }}</a></li>
+    </ul>
   </div>
-</template>
+</header>
+  </template>
 
 <script>
+/* eslint-disable */
 export default {
-  name: 'HeaderComics'
+  name: 'HeaderComics',
+  data () {
+    return {
+      activeIndex: 1,
+      links: [{
+        text: 'CHARACTERS'
+      },
+      {
+        text: 'COMICS'
+      },
+      {
+        text: 'MOVIES'
+      },
+      {
+        text: 'TV'
+      },
+      {
+        text: 'GAMES'
+      },
+      {
+        text: 'COLLECTIBLES'
+      },
+      {
+        text: 'VIDEOS'
+      },
+      {
+        text: 'FANS'
+      },
+      {
+        text: 'NEWS'
+      },
+      {
+        text: 'SHOP'
+      }]
+    }
+  },
+  methods: {
+    setIndex(index) {
+      this.activeIndex = index;
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    img {
+      width: 60px;
+    }
+  }
 
+  .active_index {
+    border-bottom: solid 3px #0282F9;
+  }  
+
+  li {
+    display: inline-block;
+    line-height: 5rem;
+    a {
+      line-height: 1rem;
+      padding: 2rem .5rem;
+      text-decoration: none;
+      font-size: 10px;
+    }
+  }
 </style>
