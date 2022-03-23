db657211beb025c3ec4fca2889e9fed66b89255b
xVal4k <cristianbricicaru.cb@gmail.com>
2022-03-23 14:02:20 +0100

    gamecards structure

---
+++ src/components/GameCards.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img :src="cardSrc" :alt="cardTitle">
+    <h4>{{ cardTitle }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameCards',
+  props: {
+    cardSrc: String,
+    cardTitle: String
+  }
+}
+</script>
+
+<style>
+
+</style>
